a6f8f864f1266f93288077b72b43549506927ce4
Rapheal <nhuxhr@gmail.com>
2025-03-11 18:33:29 +0100

    feat: impl `@nestjs/config` & example env file

---
+++ @types/cfg.d.ts
@@ -0,0 +1,3 @@
+interface Cfg {
+  PORT: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import consola from 'consola';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 // Merge the console object with the consola object
 Object.assign(console, consola);
 
 async function bootstrap() {
+  // Create a new NestJS application instance
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Get the cfg service
+  const cfg = app.get(ConfigService<Cfg>);
+
+  // Get the port from environment variables, default to 3000 if not set
+  const port = parseInt(cfg.getOrThrow('PORT'), 10) ?? 3000;
+
+  // Start listening on the specified port
+  await app.listen(port);
+
+  // Log success message
+  console.success(`Server running on http://localhost:${port}`);
 }
 
 void bootstrap();

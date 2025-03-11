407a7f02e2356b65881b445eb88c39758bfc4401
Rapheal <nhuxhr@gmail.com>
2025-03-11 22:14:08 +0100

    chore: enhance `UserEntity`

--- src/domain/user/entity/user.entity.ts
+++ src/domain/user/entity/user.entity.ts
@@ -1,16 +1,31 @@
-import { Exclude } from 'class-transformer';
+import { Exclude, instanceToPlain } from 'class-transformer';
+import { IsDate, IsEmail, IsString, IsUUID } from 'class-validator';
 
 export class UserEntity {
+  @IsUUID()
   id: string;
+
+  @IsDate()
   createdAt: Date;
+
+  @IsDate()
   updatedAt: Date;
+
+  @IsString()
   name: string;
+
+  @IsEmail()
   email: string;
 
+  @IsString()
   @Exclude()
   password: string;
 
   constructor(partial: Partial<UserEntity>) {
     Object.assign(this, partial);
+  }
+
+  toJSON() {
+    return instanceToPlain(this);
   }
 }

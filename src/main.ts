1b02eefcae6312eee72dde9fdb6ff2bc5cdbedd6
Rapheal <nhuxhr@gmail.com>
2025-03-11 18:23:06 +0100

    feat: integrate `consola` for enhanced logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import consola from 'consola';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+// Merge the console object with the consola object
+Object.assign(console, consola);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();

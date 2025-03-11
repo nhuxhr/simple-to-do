6b27d4e11b55c6216f52b8446c5f1eafc43c9d27
Rapheal <nhuxhr@gmail.com>
2025-03-11 18:57:23 +0100

    feat: impl `rest` API module

---
+++ src/api/rest/rest.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { MainModule } from './main/main.module';
+
+@Module({
+  imports: [MainModule],
+})
+export class RestModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ApiModule],
   providers: [AppService],
 })
 export class AppModule {}

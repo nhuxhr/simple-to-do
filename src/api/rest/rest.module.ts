933ac001acb3ae7c8a747a670ec351114e6e0b21
Rapheal <nhuxhr@gmail.com>
2025-03-11 20:21:06 +0100

    feat: impl rate limiting with `@nestjs/throttler`

--- src/api/rest/rest.module.ts
+++ src/api/rest/rest.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
 import { MainModule } from './main/main.module';
 
 @Module({
   imports: [MainModule],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class RestModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { seconds, ThrottlerModule } from '@nestjs/throttler';
 import { AppService } from './app.service';
 import { ModulesModule } from './modules/modules.module';
 import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ModulesModule, ApiModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: seconds(60),
+          limit: 100,
+        },
+      ],
+    }),
+    ModulesModule,
+    ApiModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
